b468f8829816d91205eb055b00cc4db7ed5e49d9
retore <44674058+retore404@users.noreply.github.com>
2020-04-18 13:08:54 +0900

    update: パスワード総当りを実装

--- excel_opener.ps1
+++ excel_opener.ps1
@@ -2,9 +2,9 @@
 ## スクリプト名：excel_opener.ps1
 ## スクリプト内容：引数で渡すcsvに記載されている文字列で
 ## 　　　　　　　　パスワード付きExcelを開けないか試すスクリプト．
-## 　　　　　　　　試行の結果は，同フォルダにログとして出力する．
+## 　　　　　　　　試行の結果は，標準出力する．
 ## 引数：対象のExcelファイルパス, パスワード候補のcsvファイルパス
-## 戻り値：なし（同フォルダにログを出力）
+## 戻り値：なし
 ######################################################################
 
 # 引数の受け取り
@@ -19,15 +19,20 @@ $password_list | Format-Table
 foreach($p in $password_list){
     try { 
         $excel = New-Object -ComObject Excel.Application
-        $excel.Visible = $true
-        $excel.DisplayAlerts = $true
+        $excel.Visible = $false
+        $excel.DisplayAlerts = $false
     
         # パスワードを指定してブックを開く
+        $wb = $null # wbを初期化
         $wb = $excel.Workbooks.Open($filepath, [Type]::Missing, [Type]::Missing, [Type]::Missing, $p.password)
         $excel.Quit()
 
-        $p.password
-    
+        # 正しいパスワードを標準出力
+        Write-Host "The correct password is " $p.password
+        break
+    } catch {
+        Write-Host $p.password "is a wrong password."
+        $excel.quit()
     } finally {
         $sheet, $wb, $excel | ForEach-Object {
             if ($_ -ne $null) {
